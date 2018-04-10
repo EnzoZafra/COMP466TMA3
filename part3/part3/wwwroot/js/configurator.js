af9c411a68b070b50cf93c2755571f361bcdb7bd
enzozafra <zafra@ualberta.ca>
2018-04-09 21:40:19 -0600

    dropdown eventlisteners wont work

---
+++ part3/part3/wwwroot/js/configurator.js
@@ -0,0 +1,36 @@
+﻿$("#processor").on('change', function() {
+    console.log($(this));
+    console.log("lol");
+});
+
+$("#motherboard").on('change', function() {
+    console.log($(this));
+});
+
+$("#ram").on('change', function() {
+    console.log($(this));
+});
+
+$("#harddrive").on('change', function() {
+    console.log($(this));
+});
+
+$("#videocard").on('change', function() {
+    console.log($(this));
+});
+
+$("#powersupply").on('change', function() {
+    console.log($(this));
+});
+
+$("#soundcard").on('change', function() {
+    console.log($(this));
+});
+
+$("#os").on('change', function() {
+    console.log($(this));
+});
+
+function updateTotal() {
+// TODO
+}
