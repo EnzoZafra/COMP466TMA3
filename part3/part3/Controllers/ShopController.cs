ae9cdfb873ec94263e9d50ffb5ccd0cfe0355970
enzozafra <zafra@ualberta.ca>
2018-04-11 12:35:25 -0600

    event handlers all work

--- part3/part3/Controllers/ShopController.cs
+++ part3/part3/Controllers/ShopController.cs
@@ -24,8 +24,8 @@ namespace part3.Controllers
                 Part part3 = new Part(++counter, "Harddrive " + i, "2 Terabyte Drive", "Harddrive", 89.99);
                 Part part4 = new Part(++counter, "RAM " + i, "16GB RAM, 2x8GB DDR4", "RAM", 224.99);
                 Part part5 = new Part(++counter, "Motherboard " + i, "4 DDR4 slots, Intel Slot, 2 PCIe Slots", "Motherboard", 129.99);
-                Part part7 = new Part(++counter, "Sound Card" + i, "7.1 Surround Sound", "SoundCard", 49.99);
-                Part part8 = new Part(++counter, "Power Supply" + i, "Gold 550w Modular", "PowerSupply", 89.99);
+                Part part6 = new Part(++counter, "Sound Card" + i, "7.1 Surround Sound", "SoundCard", 49.99);
+                Part part7 = new Part(++counter, "Power Supply" + i, "Gold 550w Modular", "PowerSupply", 89.99);
 
                 Software os = new Software(++counter, "Operating System" + i, "A Windows Operating System", 119.99);
                 software.Add(os);
@@ -35,12 +35,12 @@ namespace part3.Controllers
                 partlist.Add(part3);
                 partlist.Add(part4);
                 partlist.Add(part5);
+                partlist.Add(part6);
                 partlist.Add(part7);
-                partlist.Add(part8);
 
                 String desc = "This computer has a " + part1.getName() + ", " + part2.getName() + ", "
                                 + part3.getName() + ", " + part4.getName() + ", " + part5.getName() + ", "
-                                + part7.getName() + ", " + part8.getName() + ", " + os.getName();
+                                + part6.getName() + ", " + part7.getName() + ", " + os.getName();
                 Computer comp = new Computer("Computer " + i, partlist, os, desc);
 
                 prebuilt.Add(comp);
@@ -49,8 +49,8 @@ namespace part3.Controllers
                 hardware.Add(part3);
                 hardware.Add(part4);
                 hardware.Add(part5);
+                hardware.Add(part6);
                 hardware.Add(part7);
-                hardware.Add(part8);
 
             }
 
--- part3/part3/wwwroot/js/configurator.js
+++ part3/part3/wwwroot/js/configurator.js
@@ -1,36 +1,141 @@
-﻿$("#processor").on('change', function() {
-    console.log($(this));
-    console.log("lol");
+﻿prices = {}
+selectedPrice = {}
+initStore()
+updateTotal()
+storePrices("test")
+
+$("#processor").on('change', function() {
+    selectedPrice["cpu"] = parseFloat(prices[$(this).val()] || 0);
+    var price = selectedPrice["cpu"]
+    if (price == 0) { 
+        document.getElementById("cpuprice").innerHTML = "$" + price + ".00";
+    }
+    else {
+        document.getElementById("cpuprice").innerHTML = "$" + price;
+    }
+    updateTotal()
 });
 
 $("#motherboard").on('change', function() {
-    console.log($(this));
+    selectedPrice["mb"] = parseFloat(prices[$(this).val()] || 0);
+    var price = selectedPrice["mb"]
+    if (price == 0) { 
+        document.getElementById("mbprice").innerHTML = "$" + price + ".00";
+    }
+    else {
+        document.getElementById("mbprice").innerHTML = "$" + price;
+    }
+    updateTotal()
 });
 
 $("#ram").on('change', function() {
-    console.log($(this));
+    selectedPrice["ram"] = parseFloat(prices[$(this).val()] || 0);
+    var price = selectedPrice["ram"]
+    if (price == 0) { 
+        document.getElementById("ramprice").innerHTML = "$" + price + ".00";
+    }
+    else {
+        document.getElementById("ramprice").innerHTML = "$" + price;
+    }
+    updateTotal()
 });
 
 $("#harddrive").on('change', function() {
-    console.log($(this));
+    selectedPrice["hdd"] = parseFloat(prices[$(this).val()] || 0);
+    var price = selectedPrice["hdd"]
+    if (price == 0) { 
+        document.getElementById("hddprice").innerHTML = "$" + price + ".00";
+    }
+    else {
+        document.getElementById("hddprice").innerHTML = "$" + price;
+    }
+    updateTotal()
 });
 
 $("#videocard").on('change', function() {
-    console.log($(this));
+    selectedPrice["gpu"] = parseFloat(prices[$(this).val()] || 0);
+    var price = selectedPrice["gpu"]
+    if (price == 0) { 
+        document.getElementById("gpuprice").innerHTML = "$" + price + ".00";
+    }
+    else {
+        document.getElementById("gpuprice").innerHTML = "$" + price;
+    }
+    updateTotal()
 });
 
 $("#powersupply").on('change', function() {
-    console.log($(this));
+    selectedPrice["psu"] = parseFloat(prices[$(this).val()] || 0);
+    var price = selectedPrice["psu"]
+    if (price == 0) { 
+        document.getElementById("psuprice").innerHTML = "$" + price + ".00";
+    }
+    else {
+        document.getElementById("psuprice").innerHTML = "$" + price;
+    }
+    updateTotal()
 });
 
 $("#soundcard").on('change', function() {
-    console.log($(this));
+    selectedPrice["sc"] = parseFloat(prices[$(this).val()] || 0);
+    var price = selectedPrice["sc"]
+    if (price == 0) { 
+        document.getElementById("scprice").innerHTML = "$" + price + ".00";
+    }
+    else {
+        document.getElementById("scprice").innerHTML = "$" + price;
+    }
+    updateTotal()
 });
 
 $("#os").on('change', function() {
-    console.log($(this));
+    selectedPrice["os"] = parseFloat(prices[$(this).val()] || 0);
+    var price = selectedPrice["os"]
+    if (price == 0) { 
+        document.getElementById("osprice").innerHTML = "$" + price + ".00";
+    }
+    else {
+        document.getElementById("osprice").innerHTML = "$" + price;
+    }
+    updateTotal()
 });
 
 function updateTotal() {
-// TODO
+    var total = selectedPrice["cpu"] + selectedPrice["mb"]
+                + selectedPrice["ram"] + selectedPrice["hdd"]
+                + selectedPrice["gpu"] + selectedPrice["psu"]
+                + selectedPrice["sc"] + selectedPrice["os"]
+                
+    if (total == 0) { 
+        document.getElementById("totalprice").innerHTML = "$" + total + ".00";
+    }
+    else {
+        document.getElementById("totalprice").innerHTML = "$" + total;
+    }
+}
+
+function storePrices(obj) {
+    var counter = 0;
+    for (var i = 0; i < 5; i++) 
+    {
+        prices[++counter] = 499.99
+        prices[++counter] = 374.99
+        prices[++counter] = 89.99
+        prices[++counter] = 224.99
+        prices[++counter] = 129.99
+        prices[++counter] = 49.99
+        prices[++counter] = 89.99
+        prices[++counter] = 119.99
+    }
+}
+
+function initStore() {
+    selectedPrice["cpu"] = 0
+    selectedPrice["mb"] = 0
+    selectedPrice["ram"] = 0
+    selectedPrice["gpu"] = 0
+    selectedPrice["hdd"] = 0
+    selectedPrice["psu"] = 0
+    selectedPrice["sc"] = 0
+    selectedPrice["os"] = 0
 }
