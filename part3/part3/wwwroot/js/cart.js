4c6e47d734858d1de91854af7618d000929280ec
enzozafra <zafra@ualberta.ca>
2018-04-13 14:18:25 -0600

    can clear cart

--- part3/part3/wwwroot/js/cart.js
+++ part3/part3/wwwroot/js/cart.js
@@ -1,10 +1,22 @@
 ﻿function clearCart() 
 {
+   var val = readCookie("cart")
    del_cookie("cart")
    location.reload();
 }
 
 function del_cookie(name) 
 {
-    document.cookie = name+'=; Max-Age=-99999999;';  
-};
+    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
