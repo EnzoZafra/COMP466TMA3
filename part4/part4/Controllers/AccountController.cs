10cff6b8b2c56206179923fd2d24be59fd9c20e9
enzozafra <zafra@ualberta.ca>
2018-04-26 17:33:26 -0600

    start login

---
+++ part4/part4/Controllers/AccountController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using part4.Data;
+using part4.Models;
+
+namespace part4.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly StoreContext _context;
+
+        public AccountController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            // For testing
+            return View(_context.Users.ToList());
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(User user)
+        {
+            if (ModelState.IsValid) {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.Message = user.Username + " successfully registered";
+            return View();
+        }
+
+        // Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(User user) {
+            var usr = _context.Users.Single(u => u.Username == user.Username 
+                                            && u.Password == user.Password);
+            if (usr != null) {
+                Response.Cookies.Delete("UserID");
+                Response.Cookies.Delete("Username");
+                Response.Cookies.Append("UserID", usr.UserId.ToString());
+                Response.Cookies.Append("Username", user.Username);
+                return RedirectToAction("LoggedIn");
+            }
+            else {
+                ModelState.AddModelError("", "Username or password is incorrect");
+            }
+            return View();
+        }
+
+        public IActionResult LoggedIn() {
+            if (Request.Cookies["UserID"] != null) {
+                return RedirectToAction("Index", "Home");
+            }
+            else {
+                return RedirectToAction("Login");
+            }
+        }
+    }
+}
--- part4/part4/Models/User.cs
+++ part4/part4/Models/User.cs
@@ -1,14 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace part4.Models
 {
     public class User
     {
         public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Username required")]
         public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password required")]
+        [DataType(DataType.Password)] 
         public string Password { get; set; }
+
+        [NotMapped]
+        [Required(ErrorMessage = "Please confirm your password")]
+        [DataType(DataType.Password)] 
+        public string ConfirmPassword { get; set; }
+
+        [Required(ErrorMessage = "Email required")]
+        [DataType(DataType.EmailAddress)] 
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Security Question required")]
         public string Question { get; set; }
+
+        [Required(ErrorMessage = "Security Answer required")]
         public string Answer { get; set; }
 
         public ICollection<Order> Orders { get; } = new List<Order>();
